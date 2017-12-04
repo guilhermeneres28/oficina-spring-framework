57cfe9265d0bc217226e6967d9f56aa6eb3d8117
Guilherme Neres <guilherme.neres.henrique@gmail.com>
2017-12-04 13:29:55 -0200

    Disponibilizando object de Vinho para view de cadastro

--- src/main/java/com/algaworks/vinhos/controller/VinhosController.java
+++ src/main/java/com/algaworks/vinhos/controller/VinhosController.java
@@ -3,14 +3,19 @@ package com.algaworks.vinhos.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.vinhos.model.Vinho;
 
 @Controller
 @RequestMapping("/vinhos")
 public class VinhosController {
 	
 	@GetMapping("/novo")
-	public String novo() {
-		return "vinhos/cadastro-vinho";
+	public ModelAndView novo() {
+		ModelAndView modelAndView = new ModelAndView("vinhos/cadastro-vinho");
+		modelAndView.addObject("vinho", new Vinho());
+		return modelAndView;
 	}
 
 }
